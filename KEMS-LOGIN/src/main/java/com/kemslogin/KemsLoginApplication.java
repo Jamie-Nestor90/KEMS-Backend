59fa09472a211603262b0c5af25e2409b8be97c1
Jamie Yoo <naive0612@gmail.com>
2024-10-09 10:49:38 +0900

    feat : initial commit
    initial commit

---
+++ KEMS-LOGIN/src/main/java/com/kemslogin/KemsLoginApplication.java
@@ -0,0 +1,13 @@
+package com.kemslogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KemsLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KemsLoginApplication.class, args);
+	}
+
+}
